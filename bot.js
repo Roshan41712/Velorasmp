30cdf8f3cae20960c3af59f8530aeb3530b0d5c8
Roshan41712 <roshanminecraft41712@gmail.com>
2026-05-24 16:42:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,36 @@
-console.log("Velora bot online")
+
+const mineflayer = require('mineflayer')
+
+function startBot() {
+
+const bot = mineflayer.createBot({
+host: 'Velorasmptemp.aternos.me',
+port: 16037,
+username: 'AashaBot'
+})
+
+bot.on('spawn', () => {
+console.log('Bot online')
+})
+
+bot.on('playerJoined', (player) => {
+if (player.username === bot.username) return
+
+setTimeout(() => {
+bot.chat(`Welcome to VeloraSMP ❤️🫂 ${player.username}`)
+}, 2000)
+})
+
+bot.on('end', () => {
+console.log('Reconnect...')
+
+setTimeout(() => {
+startBot()
+}, 5000)
+})
+
+bot.on('error', console.log)
+
+}
+
+startBot()
